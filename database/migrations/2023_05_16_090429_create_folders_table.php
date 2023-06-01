86d5655cfca7232e359525502154a204f5862806
mizuki1006 <t23m06s17@gmail.com>
2023-06-01 14:33:41 +0900

    migration

--- database/migrations/2023_05_16_090429_create_folders_table.php
+++ database/migrations/2023_05_16_090429_create_folders_table.php
@@ -15,7 +15,7 @@ class CreateFoldersTable extends Migration
                 $table->id()->autoIncrement()->unsigned();
                 $table->string('title', 20);
                 $table->timestamps();
-                $table->unsignedBigInteger('user_id')->unsigned();
+                $table->bigInteger('user_id')->unsigned();
                 // 外部キーを設定する
                 $table->foreign('user_id')->references('id')->on('users');
             });
